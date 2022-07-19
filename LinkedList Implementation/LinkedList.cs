1571b7dc1d717cfe1747522d6c76157a983e0e78
Aman11 <kadakuntlaaman@gmail.com>
2022-07-19 16:32:45 +0530

    first commit

---
+++ LinkedList Implementation/LinkedList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Implementation
+{
+    internal class LinkedList
+    {
+        private Node head = null;
+        public void Add(int val)
+        {
+            Node newNode = new Node();
+            newNode.val = val;
+            newNode.link = null;
+            if (head == null)
+            {
+                head = new Node();
+                head.link = newNode;
+            }
+            else
+            {
+                Node n = head;
+                while (n.link != null)
+                {
+                    n = n.link;
+                }
+                n.link = newNode;
+            }
+        }
+        public void Search(int val)
+        {
+            Node n = head.link;
+            bool isPresent = false;
+            while(n!=null)
+            {
+                if(n.val==val)
+                {
+                    isPresent = true;
+                    break;
+                }
+                n = n.link;
+            }
+            if (isPresent)
+                Console.WriteLine("found " + val);
+            else
+                Console.WriteLine(val + " not found");
+        }
+        public void Remove(int val)
+        {
+            Node previousLink = head;
+            Node currenNode = previousLink.link;
+            bool isPresent = false;
+            while(currenNode!=null)
+            {
+                if(currenNode.val==val)
+                {
+                    Node n=currenNode.link;
+                    previousLink.link =n;
+                    isPresent = true;
+                    break;
+                }
+                previousLink = currenNode;
+                currenNode = currenNode.link;
+            }
+            if (!isPresent)
+                Console.WriteLine(val + " not found");
+        }
+        public void Display()
+        {
+            Node n = head.link;
+            while (n != null)
+            {
+                Console.Write(n.val + " ");
+                n = n.link;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LinkedList Implementation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.Add(4);
+            linkedList.Add(5);
+            linkedList.Display();
+            linkedList.Search(4);
+            linkedList.Search(10);
+            linkedList.Remove(4);
+            linkedList.Display();
+            linkedList.Remove(10);
+            linkedList.Add(6);
+            linkedList.Display();
+            linkedList.Remove(1);
+            linkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
